b5711c626ab9d89932c6a4e889eff61ab087f7c0
Titouan Herbaut <titouan.herbaut@etu.iut-tlse3.fr>
2024-03-12 10:50:07 +0100

    ajout de try/catch pour la bd

--- src/enregistrer.php
+++ src/enregistrer.php
@@ -10,15 +10,26 @@
         $pseudo = $_GET['pseudo'];
         $phrase = $_GET['phrase'];
 
-        $connection = new PDO("mysql:host=$server;dbname=$db", $login, $mdp);
+        //Connexion a la base de donnees
+        try{
+            $connection = new PDO("mysql:host=$server;dbname=$db", $login, $mdp);
 
-        $query = $connection->prepare("INSERT INTO messages(contenu, userPseudo, horaire) VALUES(:phrase, :pseudo, :heure)");
+            try{
+                $query = $connection->prepare("INSERT INTO messages(contenu, userPseudo, horaire) VALUES(:phrase, :pseudo, :heure)");
 
-        $query->bindparam(':phrase', $phrase);
-        $query->bindparam(':pseudo', $pseudo);
-        $query->bindparam(':heure', now());
+                $query->bindparam(':phrase', $phrase);
+                $query->bindparam(':pseudo', $pseudo);
+                $query->bindparam(':heure', now());
 
-        $query->execute();
+                $query->execute();
+            }catch(PDOException $e){
+                echo "Erreur d'execution de la requete";
+            }
+
+        }catch(PDOException $e){
+            echo "Erreur de connexion a la base de donnees";
+        }
+
     }
 
 ?>
