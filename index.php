2f7275835681320d880a32b7bf3ac741bd68dca8
Titouan Herbaut <titouan.herbaut@etu.iut-tlse3.fr>
2024-03-23 23:47:27 +0100

    affichage global a peu pres propre

--- index.php
+++ index.php
@@ -10,45 +10,43 @@
 
         // Fonction d'envoi du message
         function enregistrerMessage(){
+            // Recupere le pseudo et le message dans les inputs
             var pseudo = $("#pseudo").val();
             var phrase = $("#message").val();
+            // Envoi des données à enregistrer.php
             $.get("src/enregistrer.php", {pseudo: pseudo, phrase: phrase}, function(data){
                 $("#message").val(''); // Efface le champ de saisie
             });
         }
 
-        
+        // Fonction de récupération et d'affichage des messages
+        // Avec listId en paramètre qui contient les id des messages déjà affichés
         function recupererMessages(listId){
+            // Récupération des messages
             $.getJSON("src/recuperer.php", function(data){
+                // Parcours des messages en sens inverse
                 $.each(data.reverse(), function(index, message){
+                    // Si le message n'est pas déjà affiché
                     if(!(listId.includes(message['idMessage']))){
-                        console.log("id du dernier message : " + message['idMessage']);
-                        console.log("liste des id : " + listId);
+
+                        // Ajout d'un séparateur entre chaque message sauf pour le premier
                         if (!(index === 0)){
                             $(".messages").append("<hr>");
                         }
-                        $(".messages").append("<strong>" + message['userPseudo'] + "</strong> : "+message['horaire']+"<br> " + message['contenu']);
+
+                        // Affichage de chaque message
+                        var date = new Date(message['horaire']);
+                        str = date.toLocaleString();
+                        $(".messages").append("<strong>" + message['userPseudo'] + "</strong> : "+ str +"<br>" + message['contenu']);
                         listId.push(message['idMessage']);
+                        // Scroll automatique vers le bas du bloc
+                        document.getElementById('msg').scrollBy(0, 1000);
                     }
                 })
             })
         }
-/*
-        function getLastMessage(){
-            $.getJSON("src/recuperer.php", function(data){
-                $.each(data.reverse(), function(index, message){
-                    var id = []; // Tableau contenant les id des messages
-                    for(var i = 0; i <= id.length; i++){
-                        if(!(message['idMessage'] === id[i])){
-                            $(".messages").append("<hr>");
-                            $(".messages").append("<strong>" + message['userPseudo'] + "</strong> : "+message['horaire']+"<br> " + message['contenu']);
-                            id.push(message['idMessage']);
-                        }
-                    }
-                })
-            });
-        }
-*/
+
+
         $(document).ready(function(){
             // Envoi du message lors du click sur le bouton Envoyer
             $("#envoyer").click(function(){
@@ -66,7 +64,7 @@
 
         setInterval(function(){
             recupererMessages(listId);
-        }, 2000);
+        }, 1000);
 
     </script>
 </head>
@@ -76,8 +74,10 @@
         <input type="text" name="pseudo" id="pseudo" placeholder="Entrez votre pseudo">
     </div>
     <div class="liste-messages">
-        <span class="messages">
-        </span>
+        <div class="display-messages" id="msg">
+            <span class="messages">
+            </span>
+        </div>
     </div>
     <div class="input-message">
         <input type="text" name="message" id="message" placeholder="Entrez votre message">
